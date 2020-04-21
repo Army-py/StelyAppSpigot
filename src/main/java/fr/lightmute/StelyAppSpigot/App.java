a212e26a40e73def21586982ee2bc4bbf785cccc
LightMute <dylanbusque10@gmail.com>
2020-04-21 11:05:04 +0200

    Firtsave

---
+++ src/main/java/fr/lightmute/StelyAppSpigot/App.java
@@ -0,0 +1,13 @@
+package fr.lightmute.StelyAppSpigot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class App extends JavaPlugin {
+
+	public void onEnable() {
+
+	}
+	public void onDisable() {
+
+	}
+}
